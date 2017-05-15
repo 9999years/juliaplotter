4af42ab6a1bb89ade0a2be8e4562d97831fd7028
Rebecca Turner <637275@gmail.com>
2017-05-14 22:39:12 -0400

    mostly working, no readme yet though

--- julia.py
+++ julia.py
@@ -1,5 +1,6 @@
 import time
 import math
+import cmath
 import re
 import codecs
 import sys
@@ -22,12 +23,12 @@ math.csc = csc
 math.cot = cot
 math.sign = sign
 
-def eval_fn(x):
+def eval_fn(z, c):
     global fn
     ret: float
     try:
         ret = eval(fn)
-    except ValueError:
+    except (ValueError, OverflowError, ZeroDivisionError):
         # negative number in a log or root probably
         ret = float("nan")
     return ret
@@ -41,45 +42,31 @@ def scale(val,  valmin,  valmax,  outmin,  outmax):
 
 def gtsX(x):
     global graph, width
-    return scale(x, graph.x.min, graph.x.max, 0, width)
+    return scale(x, graph['x']['min'], graph['x']['max'], 0, width)
 
 def gtsY(y):
     global graph, height
-    return scale(y, graph.y.min, graph.y.max, 0, height)
+    return scale(y, graph['y']['min'], graph['y']['max'], 0, height)
 
 def stgX(x):
     global graph, width
-    return scale(x, 0, width, graph.x.min, graph.x.max)
+    return scale(x, 0, width, graph['x']['min'], graph['x']['max'])
 
 def stgY(y):
     global graph, height
-    return scale(y, 0, height, graph.y.min, graph.y.max)
+    return scale(y, 0, height, graph['y']['min'], graph['y']['max'])
 
-print(
-"enter a formula to plot in terms of z and c ({z, c} ∈ ℂ) or HELP for help"
-)
+def clamp(v, lo, hi):
+    return max(min(v, hi), lo)
+
+print("enter a formula to plot in terms of z and c ({z, c} ∈ ℂ)")
 
 def get_fn():
     return input("z_n+1(z, c) := ")
 
-fn = get_fn()
+orig_fn = fn = get_fn()
 if len(fn) is 0:
     fn = "z^2 + c"
-
-while fn == "HELP":
-    print("""enter a formula to graph in terms of x
-functions like sin, tan, ln, floor, and sqrt are allowed
-as are pi and e. exponentiation is allowed (e.g. x^2) but for non-integer
-exponents e.g. pow(x, 2.2) should be used instead. parsing is not that advanced,
-so don’t get too surprised if something wacky happens. as with c; when in
-doubt, add more parenthesis. (sinx^2 will parse as sin(x)^2, for example, and
-sin^2 x will probably crash the program)
-EXAMPLE: y(x) = 2sinx
-EXAMPLE: y(x) = ln(0.5x^2)
-EXAMPLE: y(x) = (x - 3)(x)(x + 2) / 10
-EXAMPLE: y(x) = sqrt(4 - (abs(fmod(x, 4)) - 2)^2) * sign(fmod(x, 8) - 4sign(x))""")
-    fn = get_fn()
-
 
 # replace stuff like 2tan(4x) with 2*tan(4*x)
 fn = re.sub(r"(\d+)([a-zA-Z]+)", r"\1 * \2", fn)
@@ -90,20 +77,21 @@ fn = re.sub(r"ln", r"log", fn)
 # when you type (x + 2)(x - 2) you probably meant to multiply them right?
 fn = re.sub(r"\)\s+\(", r")*(", fn)
 
-# replace stuff like tan x or tanx with tan(x), which python can evaluate
-# sorry this is so long, please ignore, it's literally every 1-arg math method
-fn = re.sub(r"""\b(ceil|fabs|factorial|floor|frexp|fsum|isfinite|
-|isinf|isnan|modf|trunc|exp|expm1|log|log1p|log2|log10|sqrt|acos|asin|atan|cos|
-|sin|tan|degrees|radians|acosh|asinh|atanh|cosh|sinh|tanh|erf|erfc|gamma|lgamma|
-|sec|csc|cot)\s*x\b""", r"\1(x)", fn)
+fn = re.sub(r"π", r"pi", fn)
 
-# replace stuff like tan(x) with math.tan(x), which python can evaluate sorry
-# this is so long, please ignore, it's literally every math method
-fn = re.sub(r"""\b(ceil|copysign|fabs|factorial|floor|fmod|frexp|fsumg|gcd|
-|isclose|isfinite|isinf|isnan|ldexp|modf|trunc|exp|expm1|log|log1p|log2|log10|
-|pow|sqrt|acos|asin|atan|atan2|cos|hypot|sin|tan|degrees|radians|acosh|asinh|
-|atanh|cosh|sinh|tanh|erf|erfc|gamma|lgamma|pi|e|tau|inf|nan|sec|csc|cot)\b""",
-r"math.\1", fn)
+fn = re.sub(r"(?<!cmath\.)\b(pi|e|tau|inf|infj|nan|nanj)\b", r"cmath.\1", fn)
+
+# (?<! ...)
+
+# sinz, sin z, cos c, etc.
+fn = re.sub(r"""(?<!cmath\.)\b(phase|polar|exp|log10|sqrt|acos|asin|atan|cos|
+|sin|tan|acosh|asinh|atanh|cosh|sinh|tanh|isfinite|isinf|isnan|log|
+|rect)\s*([zc])\b""", r"\1(\2)", fn)
+
+# sin(z) ...
+fn = re.sub(r"""(?<!cmath\.)\b(phase|polar|exp|log10|sqrt|acos|asin|atan|cos|
+|sin|tan|acosh|asinh|atanh|cosh|sinh|tanh|isfinite|isinf|isnan|log|rect|
+|isclose)\(""", r"cmath.\1(", fn)
 
 # so stuff like log(x)sin(x) works as expected
 fn = re.sub(r"(\w|\))\s+(\w|\()", r"\1 * \2", fn)
@@ -111,30 +99,123 @@ fn = re.sub(r"(\w|\))\s+(\w|\()", r"\1 * \2", fn)
 # replace ^ with **
 fn = re.sub(r"\^", r"**", fn)
 
-width      = max(int(input("graph width? (default 1000):  ") or 1000), 1)
-height     = max(int(input("graph height? (default 1000): ") or 1000), 1)
-iterations = max(int(input("iterations? (default 32):     ") or 32),   1)
-dom        =         input("domain? (default -3 to 3):    ") or "-3 to 3"
-ran        =         input("range?  (default -3 to 3):    ") or "-3 to 3"
+c          =         input("c? (default 0):              ") or "0"
+width      = max(int(input("graph width?  (default 500): ") or 500), 1)
+height     = max(int(input("graph height? (default 500): ") or 500), 1)
+iterations = max(int(input("iterations? (default 32):    ") or 32),  1)
+center     =         input("center? (default 0 0):       ") or "0 0"
+zoom       =         input("zoom?  (default 1):          ") or "1"
+
+c = re.sub("\s", "", c)
+c = re.sub("i", "j", c)
+c = complex(c)
+
+cutoff = 30
+
+inx = center.find(" ")
+c_x = float(center[:inx])
+c_y = float(center[(inx + 1):])
+spread = 1 / float(zoom)
 
 graph = {
-    'x': {
-        'min': float(dom[0:dom.find("to")]),
-        'max': float(dom[(dom.find("to") + 2):]),
-        'c': 0
+    "x": {
+        "min": c_x - spread,
+        "max": c_x + spread,
+        "c": 0
     },
-    'y': {
-        'min': float(ran[0:ran.find("to")]),
-        'max': float(ran[(ran.find("to") + 2):]),
-        'c': 0
+    "y": {
+        "min": c_y - spread,
+        "max": c_y + spread,
+        "c": 0
     }
 }
 
-graph.x.c = (graph.x.max + graph.x.min) / 2
-graph.y.c = (graph.y.may + graph.y.min) / 2
+graph['x']['c'] = (graph['x']['max'] + graph['x']['min']) / 2
+graph['y']['c'] = (graph['y']['max'] + graph['y']['min']) / 2
+
+def write_pixel(r, g, b, f):
+    f.write(bytes([r, g, b]))
 
 # unix_time.ppm
-with open(str(int(time.time())) + '.ppm', 'w') as out:
+fname_base = str(int(time.time()))
+with open("./info/" + fname_base + ".txt", "w") as out:
+    out.write("z_n+1 = " + orig_fn)
+    out.write("\nc = " + str(c.real) + " + " + str(c.imag) + "i")
+    out.write("\nrendered area: ("
+        + str(graph['x']['min']) + ", " + str(graph['y']['min']) + ") to ("
+        + str(graph['x']['max']) + ", " + str(graph['y']['max']) + ")"
+    )
+    out.write("\niterations: " + str(iterations))
+
+with open("./output/" + fname_base + ".ppm", "wb") as out:
+    out.write(bytes("P6\n"
+        + str(width)  + " " + str(height) + "\n255\n",
+        encoding="ascii")
+    )
+    z: complex
+    z_p: complex
+    i: int
+    color: float
+    graphx: float
+    # render columns, rows
     for x in range(0, width):
+        print(str(100 * x / width) + "% done", end="\r")
+        graphx = stgX(x)
         for y in range(0, height):
-            pass
+            # get z_0 from x (real) and y (imaginary) axis
+            z_p = z = graphx + stgY(y)*1j
+            i = 0
+            # smooth coloring using exponents????
+            color = math.exp(-abs(z))
+            # print("z_0 = " + str(z))
+            for i in range(0, iterations):
+                z = eval_fn(z, c)
+                if cmath.isnan(z) or not cmath.isfinite(z):
+                    # oh no
+                    z = 1
+                    break
+                elif abs(z) > cutoff:
+                    break
+                z_p = z
+                color += math.exp(-abs(z))
+            # circle of radius 256 centered around (256, 0)
+            # basically it makes the image brighter
+            # color = int(math.sqrt(
+                # 65536 - math.pow(256 * color / iterations - 256, 2)
+            # ))
+            # color = int(255 * color / iterations)
+            # color = (i + 1
+                # + math.log(
+                    # max(math.log(abs(z)), 1)
+                # ) / 0.693147
+            # ) / iterations
+            color /= iterations
+            # print(color)
+            # pi/2 = 1.570796
+            # multiples of π:
+            # 3.1415926535
+            # 6.283185307
+            # 9.4247779605
+            # 12.566370614
+            # 15.7079632675
+            # 18.849555921
+            # multiples of π/2:
+            # 1.57079632675
+            # 3.1415926535
+            # 4.71238898025
+            # 6.283185307
+            # 7.85398163375
+            # 9.4247779605
+            # 10.99557428725
+            # 12.566370614
+            # 14.13716694075
+            # 15.7079632675
+            # 17.27875959425
+            # 18.849555921
+            write_pixel(
+                int(255 * math.sin(color * 3.141592) ** 2),
+                int(128 * math.sin(color * 9.4247779) ** 2),
+                int(255 * math.sin(color * 15.707963) ** 2),
+                out
+            )
+    print("", end="\n")
